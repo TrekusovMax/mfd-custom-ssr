c24ba0976b7ca2684c32a5072ea4eda35f16bbfe
Maxim <maxtrekusovsite@gmail.com>
2023-06-27 23:40:53 +0300

    first commit

---
+++ 2/src/App.jsx
@@ -0,0 +1,24 @@
+export const App = ({ data }) => {
+  return (
+    <ul>
+      {data.map(({ name, mark }) => (
+        <li key={name}>
+          {name} - {mark}
+        </li>
+      ))}
+    </ul>
+  )
+}
+
+const data = [
+  { name: 'TypeScript', mark: '4.9' },
+  { name: 'JavaScript', mark: '4.8' },
+  { name: 'Go', mark: '4.7' },
+]
+
+App.getServerSideProps = () =>
+  new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(data)
+    }, 2000)
+  })
---
+++ 2/src/server.jsx
@@ -0,0 +1,29 @@
+import { readFile } from 'fs/promises'
+import { renderToStaticMarkup } from 'react-dom/server'
+import { App } from './App'
+import http from 'http'
+
+/* const app = express() */
+
+http
+  .createServer(async (req, res) => {
+    const template = await readFile('./index.html', 'utf-8')
+    const data = await App.getServerSideProps()
+    const html = renderToStaticMarkup(<App data={data} />)
+
+    res.writeHead(200, {
+      'Content-Type': 'text/html',
+    })
+
+    res.end(
+      template.replace('<div id="root"></div>', `<div id="root">${html}</div>`),
+    )
+  })
+  .listen(3000)
+/* 
+app.use(express.static('./dist'))
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000')
+})
+ */
